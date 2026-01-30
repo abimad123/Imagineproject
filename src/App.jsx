3053ee0498e08e2365fdfdee9e396a945990b17f
Abijith CG <abijithc140@gmail.com>
2026-01-30 14:09:00 +0530

    add the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router";
 
-function App() {
-  const [count, setCount] = useState(0)
+import RootLayout from "./layout/root-layout";
+import AuthLayout from "./layout/auth-layout";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Profile from "./pages/profile";
+import NotFound from "./pages/not-found";
 
-export default App
+const routes = [
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <div>About Page</div>
+      },
+      {
+        path: "auth",
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "login",
+            element: <Login />,
+          },
+          {
+            path: "register",
+            element: <Register />,
+          },
+          {
+            path: "profile",
+            element: <Profile />,
+          }
+        ]
+      },
+      {
+        path:"*",
+        element:<NotFound/>
+      }
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+const App = () => {
+  return <RouterProvider router={router}/>
+};
+
+export default App;
